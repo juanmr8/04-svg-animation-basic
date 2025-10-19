ae06de8da565422613c6a2e0300f121be00e41e5
Juan Mora Romero <moraromerojuan8@gmail.com>
2025-10-19 22:23:28 +0200

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const config: NextConfig = {
+  turbopack: {
+    root: __dirname,
+  },
 };
 
-export default nextConfig;
+export default config;
